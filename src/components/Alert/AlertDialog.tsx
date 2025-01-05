d6b70bab2a3cfe562b272a146afa2ba8eaa7d735
Anand Bhagat <anbhagat1997@gmail.com>
2025-01-05 13:40:38 +0530

    refactor: Remove unused useAlert import from AlertDialog component

--- src/components/Alert/AlertDialog.tsx
+++ src/components/Alert/AlertDialog.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import AlertComponent from "./AlertComponent";
-import { useAlert } from "./useAlert";
 
 const AlertDialog = () => {
     return <AlertComponent />;
